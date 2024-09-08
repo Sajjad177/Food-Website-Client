8b8017a60a31025f64259471e472c1c18a0e912a
sajjad <sajjadsajjad098765@gmail.com>
2024-09-09 01:38:59 +0600

    setup complete

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+
+const App = () => {
+  return (
+    <div>
+      <Button className="bg-firstColor hover:bg-hoverColor ">Click me</Button>
+    </div>
+  );
+};
+
+export default App;
